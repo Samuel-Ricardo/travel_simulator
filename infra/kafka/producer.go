28a4971ef1b8dbbe871c0c4cc65d3bcd0e43e178
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-28 11:57:07 -0300

    :sparkles: create: fun - NewKafkaProducer (infra/kafka/producer.go)

--- infra/kafka/producer.go
+++ infra/kafka/producer.go
@@ -1,3 +1,19 @@
 package kafka
 
+import (
+	"log"
+	"os"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func NewKafkaProuducer() *kafka.Producer {
+  configMap := &kafka.ConfigMap{
+    "bootstrap.servers": os.Getenv("kafkaBootstrapServers"),
+  }
+
+  producer, err := kafka.NewProducer(configMap)
+  if err != nil { log.Println(err.Error()) }
+
+  return producer;
+}
