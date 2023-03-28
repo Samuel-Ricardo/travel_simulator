7252ff4fbd8940ffdc50a7f9044c8b79f00425f9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-27 21:55:00 -0300

    :recycle: format - code (application/route/route.go)

--- application/route/route.go
+++ application/route/route.go
@@ -1,14 +1,15 @@
 package route
 
 type Route struct {
-  ID        string      `json:"routeId"`
-  ClientID  string      `json:"clientID"`
-  Positions []Position  `json:"positions"` 
+	ID        string     `json:"routeId"`
+	ClientID  string     `json:"clientID"`
+	Positions []Position `json:"positions"`
 }
 
 type Position struct {
-  Latitude  float64 `json:"latitude"`
-  Longitude float64 `json:"longitude"` 
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
 }
 
-
+type PartialRoutePosition struct {
+}
