11bdfc4181aae86357aa97b2105a738d90ccfe5e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-28 20:47:19 -0300

    :sparkles: create: fun - main (main.go)

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Samuel-Ricardo/travel_simulator
 
 go 1.16
 
-require github.com/confluentinc/confluent-kafka-go v1.9.2
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
+
+	"github.com/Samuel-Ricardo/travel_simulator/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/joho/godotenv"
 )
+
 
 func init() {
   err := godotenv.Load()
   if err != nil { log.Fatal("Error on Loading .env File") }
 }
 
+func main() {
+  
+  messageChannel := make(chan *ckafka.Message)
+  consumer := kafka.NewKafkaConsumer(messageChannel)
+
+  go consumer.Consume()
+
+  for message := range messageChannel {
+
+    fmt.Println(string(message.Value))
+  
+  }
+}
