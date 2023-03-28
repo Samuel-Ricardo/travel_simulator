6101004d3e0155d22d4ecfad96f1b7cb60c0541c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-27 22:25:47 -0300

    :sparkles: create: fun - LoadPositions (application/route/route.go)

--- application/route/route.go
+++ application/route/route.go
@@ -1,5 +1,13 @@
 package route
 
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Route struct {
 	ID        string     `json:"routeId"`
 	ClientID  string     `json:"clientID"`
@@ -19,7 +27,36 @@ type PartialRoutePosition struct {
 }
 
 func NewRoute() *Route {
-  return &Route{}
+	return &Route{}
 }
 
+func (r *Route) LoadPositions() error {
+	
+  if r.ID == "" {	return errors.New("route id not informed")	}
 
+  file, error := os.Open("destinatios/"+ r.ID +".txt")
+  if error != nil { return error }
+
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+
+    data := strings.Split(scanner.Text(), ",")
+    
+    latitude, error := strconv.ParseFloat(data[0], 64)
+    if error != nil { return nil }
+    
+    longitude, err := strconv.ParseFloat(data[1], 64)
+    if err != nil { return nil }
+
+    r.Positions = append(
+      r.Positions, 
+      Position{
+      	Latitude:  latitude,
+      	Longitude: longitude,
+      })
+  }
+
+  return nil
+}
