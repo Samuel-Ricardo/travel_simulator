dd8362108c3675126baa9eff0318584968200a45
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-28 11:48:54 -0300

    :sparkles: create: Kafka Consumer (infra/kafka/consumer.go)

--- infra/kafka/consumer.go
+++ infra/kafka/consumer.go
@@ -1,6 +1,11 @@
 package kafka
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 type KafkaConsumer struct {
@@ -11,3 +16,21 @@ func NewKafkaConsumer(messageChannel chan *kafka.Message) *KafkaConsumer {
   return &KafkaConsumer { MessageChannel: messageChannel }
 }
 
+func (k *KafkaConsumer) Consume() {
+  configMap := &kafka.ConfigMap{
+    "bootstrap.server": os.Getenv("kafkaBootstrapServers"),
+    "group.id":         os.Getenv("kafkaConsumerGroupId"),
+  }
+
+  consumer, err := kafka.NewConsumer(configMap)
+  if err != nil { log.Fatalf("Error on cosuming kafka message: "+err.Error()) }
+
+  topics := []string{os.Getenv("kafkaReadTopic")}
+  consumer.SubscribeTopics(topics, nil)
+  fmt.Println("Kafka Consumer  has been started")
+
+  for {
+    msg, err := consumer.ReadMessage(-1)
+    if err == nil { k.MessageChannel <- msg }
+  }
+}
