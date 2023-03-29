3055bc2c9dfab6c3123d658f609d9d9f1dc7095c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-29 15:41:27 -0300

    :sparkles: update: file - main.go

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
+import kafka2 "github.com/Samuel-Ricardo/travel_simulator/application/kafka"
+
 import (
 	"fmt"
 	"log"
 
-	kafka2 "github.com/Samuel-Ricardo/travel_simulator/application/kafka"
 	"github.com/Samuel-Ricardo/travel_simulator/infra/kafka"
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/joho/godotenv"
@@ -17,16 +18,14 @@ func init() {
 }
 
 func main() {
-  
+
   messageChannel := make(chan *ckafka.Message)
   consumer := kafka.NewKafkaConsumer(messageChannel)
 
   go consumer.Consume()
 
   for message := range messageChannel{
-    fmt.Println("pedro >:()")
     fmt.Println(string(message.Value)) 
-    
     go kafka2.Produce(message)
   }
 }
